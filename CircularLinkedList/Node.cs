4823b08294dc932c582f331ba51e47d153609475
Srujana-Jami <srujana.jami@innroad.com>
2020-07-31 11:41:11 +0530

    Add project files.

---
+++ CircularLinkedList/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml;
+
+namespace CircularLinkedList
+{
+    class Node
+    {
+        public int info;
+        public Node link;
+
+        public Node(int i)
+        {
+            info = i;
+            link = null;
+        }
+    }
+}
